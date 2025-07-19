f9e2ca4766aa5af95b8e405408d254dcc7f3523f
KamalSDhami <devdhami765@gmail.com>
2025-07-19 16:20:11 +0530

    add nums & maths

---
+++ 06_nums_&_maths.js
@@ -0,0 +1,29 @@
+const score = 400;
+const balance = new Number (100);
+console.log(balance);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+
+const otherNumber =23.8973
+console.log(otherNumber.toPrecision(3));
+
+const hundreads = 100000000
+console.log(hundreads.toLocaleString("en-IN"));
+
+//++++++++++++++++++++++ Maths ++++++++++++++++++++
+console.log(Math);
+console.log(Math.abs(-4))
+console.log(Math.round(4.6));
+console.log(Math.ceil(4.6));
+console.log(Math.floor(4.6));
+console.log(Math.min(4.6,4,7,4,6,));
+
+
+console.log(Math.random());
+//value at specific range
+const min=10
+const max=20
+console.log(Math.floor(Math.random()*(max-minn+1)) +min) 
+
+
